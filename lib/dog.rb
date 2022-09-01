bcfd2481bc1883326af9ac3a3bcd8006a746b971
KialoHezz <hezzykialo.official.ke@gmail.com>
2022-09-01 11:22:58 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,30 @@
-class Dog
+require 'pry'
+require 'active_record'
+
+# DB connection
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: 'db/test.sqlite'
+)
+
+# Create table attributes
+
+sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+    )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+# Inheritting a from active record
+class Dog < ActiveRecord::Base
 end
+
+# Log SQL output to the terminal
+# ActiveRecord::Base.logger = Logger.new(STDOUT)
+binding.pry
+""
